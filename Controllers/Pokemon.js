7c748c45ca4ca6f7b7569f8da7c6a2173ce0ff24
memo40 <memocann4040@gmail.com>
2021-02-09 17:22:28 +0100

    setted up pokemon by id

--- Controllers/Pokemon.js
+++ Controllers/Pokemon.js
@@ -1,4 +1,27 @@
 const db = require("../db.json")
+module.exports = {
 
-// we want to map through db and for each element of the array return the element (object)
-    
+getAll: async (_,res) => {
+      try {
+      await res.json({
+        code: 200,
+        operation: "success",
+        description: "Poke data",
+        data: db,
+      });
+    }
+    catch (e){
+      console.log(e);
+      res.sendStatus(404);
+    }
+  } ,
+getById: async (req, res) => {
+    // const poke = db.find((item) => item.name.english === "Ivysaur");
+    // res.send(db[req.params.id]);
+    const id = parseInt(req.params.id);
+    const poke = db.find((item) => item.id === id);
+    res.send(poke);
+  
+  
+  }
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const pokemonRoute = require("./Routes/pokemon");
+const pokemonRoute = require("./Routes/pokemons.js");
 
 app.use("/", pokemonRoute);
 
