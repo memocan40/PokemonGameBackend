229a275100164a69b98cf63dc0560a78a6de6b8a
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-02-10 20:08:45 +0100

    Add redirect from root (/) to /pokemons
    mainpage will now be redirected to "/pokemons" (displays all pokemons)

--- Routes/Pokemons.js
+++ Routes/Pokemons.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const pokemonControllers = require("../Controllers/pokemon");
 
+router.get("/", (_, res) => {
+  res.redirect("/pokemons");
+});
+
 router.get("/pokemons", pokemonControllers.getAll);
 router.get("/pokemons/:id", pokemonControllers.getById);
 router.get("/pokemons/:id/:info", pokemonControllers.getByIdByInfo);
