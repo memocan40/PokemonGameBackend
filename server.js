5bd42ea43fadc7f79b3f7f01fdc47a88d8e221fd
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-02-10 21:47:37 +0100

    Prepare for publishing API on Heroku
    * Add Procfile
    * Package.json: Change start script inorder to avoid errors
    * Change PORT

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
+// setting up the cors config
 app.use(cors());
 
+// import route
 const pokemonRoute = require("./Routes/pokemons.js");
 app.use("/", pokemonRoute);
 
-app.listen(port, () => console.log(`server is running on port ${port}`));
+app.listen(PORT, () =>
+  console.log(`CORS-enabled web server is running on port ${PORT}`)
+);
