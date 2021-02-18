ddc2e37753fe28b7bedcbd604846823086fc2c2a
Elie <eliegebransoued@gmail.com>
2021-02-18 12:22:57 +0100

    correcting the model once more

--- Model/Pokemon.js
+++ Model/Pokemon.js
@@ -8,11 +8,28 @@ const Schema = mongoose.Schema;
 
 const pokemonSchema = new Schema({
     
-    name :Object
-
-     });
     
+     name: {
+        english: String,
+        japanese: String,
+        chinese: String,
+        french: String
+     },
 
+
+     type: [String, String],
+
+     base: {
+        HP: Number,
+        Attack: Number,
+        Defense: Number,
+        Sp: {
+         Attack: Number,
+         Defense: Number
+        },
+        Speed: Number
+        }
+    });
     
     
 
