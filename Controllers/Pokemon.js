e2a51a316c704f03d92faef2e0c4faa552bd0112
Elie <eliegebransoued@gmail.com>
2021-02-18 15:09:20 +0100

    adding the getById controller

--- Controllers/Pokemon.js
+++ Controllers/Pokemon.js
@@ -13,25 +13,22 @@ module.exports = {
       console.log(e);
       res.sendStatus(404);
     }
+  },
+
+  getById: async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    try {
+      const dbResult = await Pokemon.find({id});
+       res.json({
+        code: 200,
+        operation: "success",
+        description: "Poke data",
+        data: dbResult,
+      });
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(404);
+    }
   }
 }
-
-  //
-  // --- getById with 'find' -----------------------------------------------
-  // getById: async (req, res) => {
-  //   const id = parseInt(req.params.id);
-  //   const pokemon = db.find((item) => item.id === id);
-
-  //   try {
-  //     await res.json({
-  //       code: 200,
-  //       operation: "succes",
-  //       description: `Pokemon found with id ${id}`,
-  //       data: pokemon,
-  //     });
-  //   } catch (e) {
-  //     console.log(e);
-  //     res.sendStatus(404);
-  //   }
-  // },
-  
